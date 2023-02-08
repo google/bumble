5a1b03fd91450305b564a383bdb7034ecaf24cc8
Gilles Boccon-Gibod <bok@bok.net>
2023-02-08 10:54:27 -0800

    format

--- apps/pair.py
+++ apps/pair.py
@@ -366,7 +366,11 @@ async def pair(
     '--request', is_flag=True, help='Request that the connecting peer initiate pairing'
 )
 @click.option('--print-keys', is_flag=True, help='Print the bond keys before pairing')
-@click.option('--keystore-file', metavar='<filename>', help='File in which to store the pairing keys')
+@click.option(
+    '--keystore-file',
+    metavar='<filename>',
+    help='File in which to store the pairing keys',
+)
 @click.argument('device-config')
 @click.argument('hci_transport')
 @click.argument('address-or-name', required=False)
