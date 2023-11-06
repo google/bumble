2cd4f84800ed598d584a0dfbd7665ca901f86b5d
Lucas Abel <22837557+uael@users.noreply.github.com>
2023-11-06 08:30:12 +0000

    pandora: add `annotations` import

--- bumble/pandora/config.py
+++ bumble/pandora/config.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
 from bumble.pairing import PairingConfig, PairingDelegate
 from dataclasses import dataclass
 from typing import Any, Dict
