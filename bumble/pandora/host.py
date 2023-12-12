bb025514e701834add3683cb8923559cb73657e8
Charlie Boutier <charliebout@google.com>
2023-12-12 22:16:59 +0000

    PandoraHost: compute advertising_interval_max with interval_range

--- bumble/pandora/host.py
+++ bumble/pandora/host.py
@@ -286,10 +286,10 @@ class HostService(HostServicer):
                 "TODO: add support for extended advertising in Bumble"
             )
         if advertising_interval := request.interval:
-            self.device.config.advertising_interval_min = advertising_interval
-            self.device.config.advertising_interval_max = advertising_interval
-        if request.interval_range:
-            raise NotImplementedError("TODO: add support for `request.interval_range`")
+            self.device.config.advertising_interval_min = int(advertising_interval)
+            self.device.config.advertising_interval_max = int(advertising_interval)
+        if interval_range := request.interval_range:
+            self.device.config.advertising_interval_max += int(interval_range)
         if request.primary_phy:
             raise NotImplementedError("TODO: add support for `request.primary_phy`")
         if request.secondary_phy:
