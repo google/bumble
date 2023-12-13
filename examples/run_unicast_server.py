76ccc8085b24cd7a400d9615f10ee4c40ff0069f
Josh Wu <joshwu@google.com>
2023-12-13 16:25:19 +0800

    ASCS: Add Source ASE operations

--- examples/run_unicast_server.py
+++ examples/run_unicast_server.py
@@ -21,8 +21,9 @@ import sys
 import os
 import struct
 import secrets
+import functools
 from bumble.core import AdvertisingData
-from bumble.device import Device, CisLink
+from bumble.device import Device
 from bumble.hci import (
     CodecID,
     CodingFormat,
@@ -39,6 +40,8 @@ from bumble.profiles.bap import (
     PacRecord,
     PublishedAudioCapabilitiesService,
     AudioStreamControlService,
+    AudioRole,
+    AseStateMachine,
 )
 from bumble.profiles.cap import CommonAudioServiceService
 from bumble.profiles.csip import CoordinatedSetIdentificationService, SirkType
@@ -70,10 +73,10 @@ async def main() -> None:
         device.add_service(CommonAudioServiceService(csis))
         device.add_service(
             PublishedAudioCapabilitiesService(
-                supported_source_context=ContextType.PROHIBITED,
-                available_source_context=ContextType.PROHIBITED,
-                supported_sink_context=ContextType.MEDIA,
-                available_sink_context=ContextType.MEDIA,
+                supported_source_context=ContextType.CONVERSATIONAL,
+                available_source_context=ContextType.CONVERSATIONAL,
+                supported_sink_context=ContextType.MEDIA | ContextType.CONVERSATIONAL,
+                available_sink_context=ContextType.MEDIA | ContextType.CONVERSATIONAL,
                 sink_audio_locations=(
                     AudioLocation.FRONT_LEFT | AudioLocation.FRONT_RIGHT
                 ),
@@ -111,73 +114,114 @@ async def main() -> None:
                         ),
                     ),
                 ],
-            )
-        )
-
-        device.add_service(AudioStreamControlService(device, sink_ase_id=[1, 2]))
-
-        advertising_data = (
-            bytes(
-                AdvertisingData(
-                    [
-                        (
-                            AdvertisingData.COMPLETE_LOCAL_NAME,
-                            bytes('Bumble LE Audio', 'utf-8'),
-                        ),
-                        (
-                            AdvertisingData.FLAGS,
-                            bytes(
-                                [
-                                    AdvertisingData.LE_GENERAL_DISCOVERABLE_MODE_FLAG
-                                    | AdvertisingData.BR_EDR_HOST_FLAG
-                                    | AdvertisingData.BR_EDR_CONTROLLER_FLAG
-                                ]
+                source_audio_locations=(AudioLocation.FRONT_LEFT),
+                source_pac=[
+                    # Codec Capability Setting 16_2
+                    PacRecord(
+                        coding_format=CodingFormat(CodecID.LC3),
+                        codec_specific_capabilities=CodecSpecificCapabilities(
+                            supported_sampling_frequencies=(
+                                SupportedSamplingFrequency.FREQ_16000
                             ),
+                            supported_frame_durations=(
+                                SupportedFrameDuration.DURATION_10000_US_SUPPORTED
+                            ),
+                            supported_audio_channel_counts=[1],
+                            min_octets_per_codec_frame=40,
+                            max_octets_per_codec_frame=40,
+                            supported_max_codec_frames_per_sdu=1,
                         ),
-                        (
-                            AdvertisingData.INCOMPLETE_LIST_OF_16_BIT_SERVICE_CLASS_UUIDS,
-                            bytes(PublishedAudioCapabilitiesService.UUID),
+                    ),
+                ],
+            )
+        )
+
+        ascs = AudioStreamControlService(
+            device,
+            sink_ase_id=[1, 2],
+            source_ase_id=[3],
+        )
+        device.add_service(ascs)
+
+        advertising_data = bytes(
+            AdvertisingData(
+                [
+                    (
+                        AdvertisingData.COMPLETE_LOCAL_NAME,
+                        bytes('Bumble LE Audio', 'utf-8'),
+                    ),
+                    (
+                        AdvertisingData.FLAGS,
+                        bytes(
+                            [
+                                AdvertisingData.LE_GENERAL_DISCOVERABLE_MODE_FLAG
+                                | AdvertisingData.BR_EDR_HOST_FLAG
+                                | AdvertisingData.BR_EDR_CONTROLLER_FLAG
+                            ]
                         ),
-                    ]
+                    ),
+                    (
+                        AdvertisingData.INCOMPLETE_LIST_OF_16_BIT_SERVICE_CLASS_UUIDS,
+                        bytes(PublishedAudioCapabilitiesService.UUID),
+                    ),
+                ]
+            )
+        )
+
+        def on_streaming(ase: AseStateMachine):
+            print('on_streaming')
+
+            async def on_cis_async():
+                print('on_cis_async')
+                subprocess = await asyncio.create_subprocess_shell(
+                    f'dlc3 | ffplay pipe:0',
+                    stdin=asyncio.subprocess.PIPE,
+                    stdout=asyncio.subprocess.PIPE,
+                    stderr=asyncio.subprocess.PIPE,
                 )
-            )
-            + csis.get_advertising_data()
-        )
-        subprocess = await asyncio.create_subprocess_shell(
-            f'dlc3 | ffplay pipe:0',
-            stdin=asyncio.subprocess.PIPE,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
 
-        stdin = subprocess.stdin
-        assert stdin
+                stdin = subprocess.stdin
+                assert stdin
 
-        # Write a fake LC3 header to dlc3.
-        stdin.write(
-            bytes([0x1C, 0xCC])  # Header.
-            + struct.pack(
-                '<HHHHHHI',
-                18,  # Header length.
-                24000 // 100,  # Sampling Rate(/100Hz).
-                0,  # Bitrate(unused).
-                1,  # Channels.
-                10000 // 10,  # Frame duration(/10us).
-                0,  # RFU.
-                0x0FFFFFFF,  # Frame counts.
-            )
-        )
+                # Write a fake LC3 header to dlc3.
+                stdin.write(
+                    bytes([0x1C, 0xCC])  # Header.
+                    + struct.pack(
+                        '<HHHHHHI',
+                        18,  # Header length.
+                        (
+                            ase.codec_specific_configuration.sampling_frequency.hz
+                            // 100
+                        ),  # Sampling Rate(/100Hz).
+                        0,  # Bitrate(unused).
+                        ase.codec_specific_configuration.audio_channel_allocation,  # Channels.
+                        (
+                            ase.codec_specific_configuration.frame_duration.us // 10
+                        ),  # Frame duration(/10us).
+                        0,  # RFU.
+                        0x0FFFFFFF,  # Frame counts.
+                    )
+                )
 
-        def on_pdu(pdu: HCI_IsoDataPacket):
-            # LC3 format: |frame_length(2)| + |frame(length)|.
-            if pdu.iso_sdu_length:
-                stdin.write(struct.pack('<H', pdu.iso_sdu_length))
-            stdin.write(pdu.iso_sdu_fragment)
+                def on_pdu(pdu: HCI_IsoDataPacket):
+                    # LC3 format: |frame_length(2)| + |frame(length)|.
+                    if pdu.iso_sdu_length:
+                        stdin.write(struct.pack('<H', pdu.iso_sdu_length))
+                    stdin.write(pdu.iso_sdu_fragment)
 
-        def on_cis(cis_link: CisLink):
-            cis_link.on('pdu', on_pdu)
+                ase.cis_link.on('pdu', on_pdu)
 
-        device.once('cis_establishment', on_cis)
+            device.abort_on('flush', on_cis_async())
+
+        for ase in ascs.ase_state_machines.values():
+            if ase.role == AudioRole.SINK:
+
+                def on_state_change(*_, ase: AseStateMachine):
+                    print(ase)
+                    if ase.state == AseStateMachine.State.STREAMING:
+                        on_streaming(ase)
+
+                ase.on('state_change', functools.partial(on_state_change, ase=ase))
 
         await device.start_extended_advertising(
             advertising_properties=(
