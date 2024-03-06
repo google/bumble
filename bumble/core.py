91320274999b3996bf3a1e9db02dbdca5d98b615
Josh Wu <joshwu@google.com>
2024-03-07 00:53:30 +0800

    MCP constants and Client implementation

--- bumble/core.py
+++ bumble/core.py
@@ -127,6 +127,10 @@ class InvalidStateError(Exception):
     """Invalid State Error"""
 
 
+class UnsupportedOperationError(Exception):
+    """Unsupported Operation Error"""
+
+
 class ConnectionError(BaseError):  # pylint: disable=redefined-builtin
     """Connection Error"""
 
