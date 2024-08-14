aa658418bcdbafa85246b5dde3d118d2ae552783
Ford Peprah <ford@squareup.com>
2024-08-14 13:58:02 -0400

    Bug: Edimax BLE Dongle Fails After Teardown and Re-Instantiation
    This patch addresses an issue where the some RTK BLE dongles fail to perform
    an HCI reset after the transport is torn down and re-instantiated. To address
    that, we prevent crashing the background threads when invalid data comes in,
    and time out if no response is received within a fixed amount of time. When
    the timeout occurs, we retry the reset, and ultimately skip over reading the
    local version information if that fails.

--- bumble/host.py
+++ bumble/host.py
@@ -514,7 +514,7 @@ class Host(AbortableEventEmitter):
         if self.hci_sink:
             self.hci_sink.on_packet(bytes(packet))
 
-    async def send_command(self, command, check_result=False):
+    async def send_command(self, command, check_result=False, response_timeout: Optional[int] = None):
         # Wait until we can send (only one pending command at a time)
         async with self.command_semaphore:
             assert self.pending_command is None
@@ -526,7 +526,8 @@ class Host(AbortableEventEmitter):
 
             try:
                 self.send_hci_packet(command)
-                response = await self.pending_response
+                await asyncio.wait_for(self.pending_response, timeout=response_timeout)
+                response = self.pending_response.result()
 
                 # Check the return parameters if required
                 if check_result:
@@ -625,14 +626,21 @@ class Host(AbortableEventEmitter):
 
     # Packet Sink protocol (packets coming from the controller via HCI)
     def on_packet(self, packet: bytes) -> None:
-        hci_packet = hci.HCI_Packet.from_bytes(packet)
+        try:
+            hci_packet = hci.HCI_Packet.from_bytes(packet)
+        except Exception as error:
+            logger.warning(f'!!! error parsing packet from bytes: {error}')
+            return
+
         if self.ready or (
             isinstance(hci_packet, hci.HCI_Command_Complete_Event)
             and hci_packet.command_opcode == hci.HCI_RESET_COMMAND
         ):
             self.on_hci_packet(hci_packet)
         else:
-            logger.debug('reset not done, ignoring packet from controller')
+            logger.debug(
+                f'reset not done, ignoring packet from controller: {hci_packet}'
+            )
 
     def on_transport_lost(self):
         # Called by the source when the transport has been lost.
